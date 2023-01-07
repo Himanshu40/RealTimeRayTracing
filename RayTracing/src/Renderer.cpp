f8a82dfcee8276924a4a0c7116cd19ecd578ce2a
Himanshu40 <himanshuwindows8.1@gmail.com>
2023-01-07 15:08:31 +0530

    Added a renderer class to render every pixel

---
+++ RayTracing/src/Renderer.cpp
@@ -0,0 +1,34 @@
+#include "Renderer.h"
+
+#include "Walnut/Random.h"
+
+void Renderer::OnResize(uint32_t width, uint32_t height)
+{
+	if (m_FinalImage) {
+		// No resize necessary
+		if (m_FinalImage->GetWidth() == width && m_FinalImage->GetHeight() == height) {
+			return;
+		}
+
+		m_FinalImage->Resize(width, height);
+	}
+	else {
+		m_FinalImage = std::make_shared<Walnut::Image>(width, height, Walnut::ImageFormat::RGBA);
+	}
+	
+	delete[] m_ImageData;
+	m_ImageData = new uint32_t[width * height];
+}
+
+void Renderer::Render()
+{
+	// Render every pixel
+	uint32_t viewportSize = m_FinalImage->GetHeight() * m_FinalImage->GetWidth();
+	for (uint32_t i = 0; i < viewportSize; ++i) {
+		m_ImageData[i] = Walnut::Random::UInt();
+		m_ImageData[i] |= 0xff000000;
+	}
+
+	m_FinalImage->SetData(m_ImageData);
+
+}
--- RayTracing/src/WalnutApp.cpp
+++ RayTracing/src/WalnutApp.cpp
@@ -2,8 +2,9 @@
 #include "Walnut/EntryPoint.h"
 
 #include "Walnut/Image.h"
-#include "Walnut/Random.h"
 #include "Walnut/Timer.h"
+
+#include "Renderer.h"
 
 using namespace Walnut;
 
@@ -25,8 +26,9 @@ public:
 		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
 		m_ViewportHeight = ImGui::GetContentRegionAvail().y;
 
-		if (m_Image) {
-			ImGui::Image(m_Image->GetDescriptorSet(), { (float)m_Image->GetWidth(), (float)m_Image->GetHeight() });
+		auto image = m_Renderer.GetFinalImage();
+		if (image) {
+			ImGui::Image(image->GetDescriptorSet(), { (float)image->GetWidth(), (float)image->GetHeight() });
 		}
 
 		ImGui::End();
@@ -38,26 +40,17 @@ public:
 
 	void Render() {
 		Timer timer;
-
-		if (!m_Image || m_ViewportHeight != m_Image->GetHeight() || m_ViewportWidth != m_Image->GetWidth()) {
-			m_Image = std::make_shared<Image>(m_ViewportWidth, m_ViewportHeight, ImageFormat::RGBA);
-			delete[] m_ImageData;
-			m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
-		}
-
-		uint32_t viewportSize = m_ViewportWidth * m_ViewportHeight;
-		for (uint32_t i = 0; i < viewportSize; ++i) {
-			m_ImageData[i] = Random::UInt();
-			m_ImageData[i] |= 0xff000000;
-		}
-
-		m_Image->SetData(m_ImageData);
 		
+		// Renderer resize
+		m_Renderer.OnResize(m_ViewportWidth, m_ViewportHeight);
+
+		// Renderer render
+		m_Renderer.Render();
+
 		m_LastRenderTime = timer.ElapsedMillis();
 	}
 private:
-	std::shared_ptr<Image> m_Image;
-	uint32_t* m_ImageData = nullptr;
+	Renderer m_Renderer;
 	uint32_t m_ViewportWidth = 0;
 	uint32_t m_ViewportHeight = 0;
 	float m_LastRenderTime = 0;
